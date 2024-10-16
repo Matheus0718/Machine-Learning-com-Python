701a73117b46dfec8f0621ff9fc86ce6f01878f2
Matheus0718 <130237235+Matheus0718@users.noreply.github.com>
2024-10-16 23:24:00 +0000

    update

---
+++ Variáveis par ML.py
@@ -0,0 +1,8 @@
+#inteiro - int
+idade = 20
+print(type(idade))
+print("hello word")
+
+#booleano
+fim_de_semana =  True
+print(type(fim_de_semana))
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-#inteiro - int
-idade = 20
-print(type(idade))
-print("hello word")
+#booleano
+fim_de_semana =  True
+print(type(fim_de_semana))
